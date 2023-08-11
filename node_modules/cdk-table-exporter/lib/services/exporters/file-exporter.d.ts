901a5ec05269e86e302ad402f44786429998e8d7
Nooh Ahamad <noohahamad@Noohs-MacBook-Pro.local>
2023-08-11 10:15:51 +0300

    newer commit

--- node_modules/cdk-table-exporter/lib/services/exporters/file-exporter.d.ts
+++
@@ -1,8 +0,0 @@
-import { Mime } from '../../mime';
-import { Exporter } from './exporter';
-export declare abstract class FileExporter<T> implements Exporter<T> {
-    constructor();
-    export(rows: Array<any>, options?: T): void;
-    abstract createContent(rows: Array<any>, options?: T): any;
-    abstract getMimeType(): Mime;
-}
--- node_modules/cdk-table-exporter/lib/services/exporters/xlsx-exporter.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { XlsExporterService } from './xls-exporter.service';
-import { Mime } from '../../mime';
-export declare class XlsxExporterService extends XlsExporterService {
-    constructor();
-    getMimeType(): Mime;
-}
